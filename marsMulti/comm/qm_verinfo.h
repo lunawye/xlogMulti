22c282a38b54d1da11198264e28aa17a64188d41
lunawye <lunawye@tencent.com>
2022-06-17 11:28:25 +0800

    20220617 可行完整版

--- marsMulti/comm/qm_verinfo.h
+++ marsMulti/comm/qm_verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f583edc"
+#define MARS_REVISION "5e90846"
 #define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-06-16 20:38:54"
+#define MARS_BUILD_TIME "2022-06-17 11:25:00"
 #define MARS_TAG ""
 
 #endif
